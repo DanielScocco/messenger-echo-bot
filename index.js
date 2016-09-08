02fd5394a9ce7069a1bbeb358575b9c7aa337437
Daniel Scocco <danielscocco@gmail.com>
2016-09-08 14:59:47 -0300

    request depenency

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express(); 
 
+var request = require('request');
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 
 app.get('/',function(req,res) {
